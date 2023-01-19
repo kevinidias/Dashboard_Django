e0e12592dddc96270e5c09c10733d22c6c1f949b
kevinidias <kevin.dias.20160@gmail.com>
2023-01-19 08:54:01 -0300

    funcao retorna_total_vnedido

--- minha_dashboard/urls.py
+++ minha_dashboard/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('retorna-total-vendido', views.retorna_total_vendido, name='retorna_total_vendido')
 ]
--- minha_dashboard/views.py
+++ minha_dashboard/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Vendas
+from django.http import JsonResponse
+from django.db.models import Sum
 
 def home(request):
     return render(request, 'home.html')
+
+def retorna_total_vendido(request):
+    total_vendas = Vendas.objects.all().aggregate(Sum('total'))['total__sum']
+    return JsonResponse({'total': total_vendas})
