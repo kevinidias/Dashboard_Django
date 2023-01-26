68884164aeab50c5579f2e6554e170159000b429
kevinidias <kevin.dias.20160@gmail.com>
2023-01-26 08:38:08 -0300

    js retorna_total_vendido

---
+++ templates/static/js/js.js
@@ -0,0 +1,9 @@
+function renderiza_total_vendido(url){
+    fetch(url, {
+        method: 'get',
+    }).then(function(result){
+        return result.json()
+    }).then(function(data){
+        document.getElementById('faturamento_total').innerHTML = data.total
+    })
+}
