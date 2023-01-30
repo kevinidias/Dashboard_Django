352edb0c81991a48635d7372f5fb9d9e1ee49af2
kevinidias <kevin.dias.20160@gmail.com>
2023-01-30 19:20:21 -0300

    funcao gerar cor

--- templates/static/js/js.js
+++ templates/static/js/js.js
@@ -7,3 +7,16 @@ function renderiza_total_vendido(url){
         document.getElementById('faturamento_total').innerHTML = data.total
     })
 }
+
+function gera_cor(qtd=1){
+    var bg_color = []
+    var border_color = []
+    for(let i = 0; i < qtd; i++){
+        let r = Math.random() * 255;
+        let g = Math.random() * 255;
+        let b = Math.random() * 255;
+        bg_color.push(`rgba(${r}, ${g}, ${b}, ${0.2})`)
+        border_color.push(`rgba(${r}, ${g}, ${b}, ${1})`)
+    }
+    return [bg_color, border_color]
+}
