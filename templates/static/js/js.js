b1bfb38e7a94ad25a5e03dba2e62bac9fc60c934
kevinidias <kevin.dias.20160@gmail.com>
2023-02-13 17:51:46 -0300

    grafico de faturamento

--- templates/static/js/js.js
+++ templates/static/js/js.js
@@ -37,3 +37,28 @@ function renderiza_despesas_mensal(){
         },
     });
 }
+
+function renderiza_faturamento_mensal(url){
+    fetch(url, {
+        method: 'get',
+    }).then(function(result){
+        return result.json()
+    }).then(function(data){
+
+        const ctx = document.getElementById('faturamento_mensal').getContext('2d');
+        var cores_faturamento_mensal = gera_cor(qtd=12)
+        const myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: data.labels,
+                datasets: [{
+                    label: 'Faturamento',
+                    data: data.data,
+                    backgroundColor: cores_faturamento_mensal[0],
+                    borderColor: cores_faturamento_mensal[1],
+                    borderWidth: 1
+                }]
+            },
+        });
+    })
+}
